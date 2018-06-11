0365033c90fd48125c414a87cd313796d77499dc
Franklin Ventura DEV-Laptop <venturafranklin@gmail.com>
2018-06-11 12:21:23 -0700

    Add models for chapters

---
+++ thetatauCMT/chapters/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from address.models import AddressField
+
+
+class Chapter(models.Model):
+    name = models.CharField(max_length=50)
+    region = models.CharField(max_length=50)
+    email = models.EmailField(_('email address'), blank=True)
+    website = models.URLField()
+    address = AddressField()
