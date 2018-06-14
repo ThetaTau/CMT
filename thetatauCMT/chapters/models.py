b485f052df59b83efc71d76d4f51a131d2733dc6
Franklin Ventura DEV-Laptop <venturafranklin@gmail.com>
2018-06-13 21:38:49 -0700

    Add region for chapter

--- thetatauCMT/chapters/models.py
+++ thetatauCMT/chapters/models.py
@@ -1,6 +1,16 @@
 from django.db import models
+from django.conf import settings
 from address.models import AddressField
 from django.utils.translation import ugettext_lazy as _
+
+
+class Region(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(_('email address'), blank=True)
+    website = models.URLField()
+    facebook = models.URLField()
+    directors = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name='region')
 
 
 class Chapter(models.Model):
