75a7769f563bfe512136574d6a91a163680f10d9
Franklin Ventura DEV-Laptop <venturafranklin@gmail.com>
2018-06-14 11:29:18 -0700

    With update to virtual environment name can be set without project

--- thetatauCMT/chapters/apps.py
+++ thetatauCMT/chapters/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class ChaptersConfig(AppConfig):
-    name = 'thetatauCMT.chapters'
+    name = 'chapters'
--- thetatauCMT/scores/apps.py
+++ thetatauCMT/scores/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class ScoresConfig(AppConfig):
-    name = 'thetatauCMT.scores'
+    name = 'scores'
