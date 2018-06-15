8d728a26e89ae24064d50f5d681a511fd00ec13b
Franklin Ventura DEV-Laptop <venturafranklin@gmail.com>
2018-06-15 09:53:12 -0700

    Practice updating users form and details view template

--- thetatauCMT/users/views.py
+++ thetatauCMT/users/views.py
@@ -22,7 +22,7 @@ class UserRedirectView(LoginRequiredMixin, RedirectView):
 
 class UserUpdateView(LoginRequiredMixin, UpdateView):
 
-    fields = ['name', ]
+    fields = ['name', 'chapter', 'major', 'graduation_year', 'phone_number', 'address']
 
     # we already imported User in the view code above, remember?
     model = User
