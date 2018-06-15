5f48a3e10319fd8603710cae4344d4a5654d2af9
Franklin Ventura DEV-Laptop <venturafranklin@gmail.com>
2018-06-15 15:30:21 -0700

    Add initial region views

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
     url(r'^accounts/', include('allauth.urls')),
 
     # Your stuff: custom urls includes go here
+    url(r'^regions/', include('regions.urls', namespace='regions')),
 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- thetatauCMT/regions/views.py
+++ thetatauCMT/regions/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse
+from django.views.generic import DetailView, ListView, RedirectView
 
-# Create your views here.
+from .models import Region
+
+
+class RegionDetailView(LoginRequiredMixin, DetailView):
+    model = Region
+    # These next two lines tell the view to index lookups by username
+    slug_field = 'slug'
+    slug_url_kwarg = 'slug'
+
+
+class RegionRedirectView(LoginRequiredMixin, RedirectView):
+    permanent = False
+
+    def get_redirect_url(self):
+        return reverse('users:detail',
+                       kwargs={'username': self.request.user.username})
+
+
+class RegionListView(LoginRequiredMixin, ListView):
+    model = Region
+    # These next two lines tell the view to index lookups by username
+    slug_field = 'slug'
+    slug_url_kwarg = 'slug'
