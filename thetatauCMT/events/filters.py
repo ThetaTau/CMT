e4f870414d67ed8020f32bc7156bbc71adf09a97
Franklin Ventura DEV-Laptop <venturafranklin@gmail.com>
2018-07-06 14:08:57 -0700

    Limit filter options for events/submissions

--- thetatauCMT/events/filters.py
+++ thetatauCMT/events/filters.py
@@ -1,11 +1,15 @@
 # filters.py
 import django_filters
 from .models import Event
+from scores.models import ScoreType
 
 
 class EventListFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(lookup_expr='icontains')
     date = django_filters.NumberFilter(name='date', lookup_expr='year')
+    type = django_filters.ModelChoiceFilter(
+        queryset=ScoreType.objects.filter(type="Evt").all())
+
     class Meta:
         model = Event
         fields = ['name', 'date',
--- thetatauCMT/submissions/filters.py
+++ thetatauCMT/submissions/filters.py
@@ -1,11 +1,15 @@
 # filters.py
 import django_filters
 from .models import Submission
+from scores.models import ScoreType
 
 
 class SubmissionListFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(lookup_expr='icontains')
     date = django_filters.NumberFilter(name='date', lookup_expr='year')
+    type = django_filters.ModelChoiceFilter(
+        queryset=ScoreType.objects.filter(type="Sub").all())
+
     class Meta:
         model = Submission
         fields = ['name', 'date',
