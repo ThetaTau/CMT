7e51a9866a56bfac392cf8a1feddef1875bdc1ce
Franklin Ventura DEV-Laptop <venturafranklin@gmail.com>
2018-07-06 07:47:01 -0700

    Attempt to add detail url view for events, does not work

--- thetatauCMT/events/urls.py
+++ thetatauCMT/events/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
         name='update'
     ),
     url(
-        regex=r'^(?P<slug>[\w.@+-]+)/$',
+        regex=r'^(?P<year>d{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2})/(?P<slug>[-w]+)/$',
         view=views.EventDetailView.as_view(),
         name='detail'
     ),
