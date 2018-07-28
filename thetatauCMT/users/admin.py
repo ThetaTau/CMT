7e1a4279b1c3bc761454a2b711084d6c38ee4eed
Franklin Ventura DEV-Laptop <venturafranklin@gmail.com>
2018-07-28 03:22:15 -0700

    Add admin for org, gpa, and service

--- thetatauCMT/users/admin.py
+++ thetatauCMT/users/admin.py
@@ -2,11 +2,15 @@ from django import forms
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
-from .models import User, UserRoleChange, UserStatusChange
+from .models import User, UserRoleChange, UserStatusChange, UserOrgParticipate,\
+    UserSemesterGPA, UserSemesterServiceHours
 
 
 admin.site.register(UserRoleChange)
 admin.site.register(UserStatusChange)
+admin.site.register(UserOrgParticipate)
+admin.site.register(UserSemesterGPA)
+admin.site.register(UserSemesterServiceHours)
 
 
 class MyUserChangeForm(UserChangeForm):
