71d53db18a0926e0cfbc38caf404fccb2c657549
Franklin Ventura DEV-Laptop <venturafranklin@gmail.com>
2018-08-08 13:16:21 -0700

    Add task name link to complete that item

--- thetatauCMT/tasks/tables.py
+++ thetatauCMT/tasks/tables.py
@@ -4,16 +4,13 @@ from .models import TaskDate
 
 
 class TaskTable(tables.Table):
-    # name = tables.LinkColumn('tasks:update',
-    #                          args=[A('pk')])
-    date = tables.DateColumn()
+    task_name = tables.LinkColumn('tasks:complete',
+                                  accessor='task.name',
+                                  args=[A('pk')])
+    date = tables.DateColumn(verbose_name="Due Date")
 
     class Meta:
         model = TaskDate
-        fields = ('task.name', 'task.owner', 'task.description', 'date')
+        fields = ('task_name', 'date', 'task.owner', 'task.description')
         attrs = {"class": "table-striped table-bordered"}
         empty_text = "There are no tasks matching the search criteria..."
-
-    # def render_date(self, record, bound_row, table):
-    #     score = record.chapter_score(self.request.user.chapter)
-    #     return score
