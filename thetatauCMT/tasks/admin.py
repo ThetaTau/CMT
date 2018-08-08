46f1149aee06c929df9671e35952f1b6c8d2bcb2
Franklin Ventura DEV-Laptop <venturafranklin@gmail.com>
2018-08-08 13:13:27 -0700

    Add admin control of tasks

--- thetatauCMT/tasks/admin.py
+++ thetatauCMT/tasks/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Task, TaskDate
 
-# Register your models here.
+
+class TaskDateInline(admin.TabularInline):
+    model = TaskDate
+    fields = ['school_type', 'date']
+    show_change_link = True
+    ordering = ['date']
+    extra = 1
+
+
+class TaskAdmin(admin.ModelAdmin):
+    inlines = [TaskDateInline]
+    list_display = ('name', 'owner', 'type')
+    list_filter = ['owner', 'type']
+    ordering = ['name',]
+
+
+admin.site.register(Task, TaskAdmin)
