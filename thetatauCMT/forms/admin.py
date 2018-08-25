f644cab2555c0c5d321fc6d9bbe795e0246777e7
Franklin Ventura DEV-Laptop <venturafranklin@gmail.com>
2018-08-25 00:56:30 -0700

    Add all the forms admins

--- thetatauCMT/forms/admin.py
+++ thetatauCMT/forms/admin.py
@@ -5,6 +5,36 @@ from .models import Badge, Guard, Initiation, Depledge, StatusChange
 
 admin.site.register(Badge)
 admin.site.register(Guard)
-admin.site.register(Initiation)
-admin.site.register(Depledge)
-admin.site.register(StatusChange)
+
+
+class InitiationAdmin(admin.ModelAdmin):
+    raw_id_fields = ['user']
+    list_display = ('user', 'date', 'created')
+    list_filter = ['date', 'created']
+    ordering = ['user',]
+    search_fields = ['user__username', 'user__name']
+
+
+admin.site.register(Initiation, InitiationAdmin)
+
+
+class DepledgeAdmin(admin.ModelAdmin):
+    raw_id_fields = ['user']
+    list_display = ('user', 'reason', 'date', 'created')
+    list_filter = ['reason', 'date', 'created']
+    ordering = ['user',]
+    search_fields = ['user__username', 'user__name']
+
+
+admin.site.register(Depledge, DepledgeAdmin)
+
+
+class StatusChangeAdmin(admin.ModelAdmin):
+    raw_id_fields = ['user']
+    list_display = ('user', 'reason', 'date_start', 'date_end', 'created')
+    list_filter = ['reason', 'date_start', 'date_end']
+    ordering = ['user',]
+    search_fields = ['user__username', 'user__name']
+
+
+admin.site.register(StatusChange, StatusChangeAdmin)
