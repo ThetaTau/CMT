0d8684ac62adf55685f0f07c535658ad4e54fdb4
Franklin Ventura DEV-Laptop <venturafranklin@gmail.com>
2018-08-31 16:04:43 -0700

    Apparently there can sometimes be no request

--- thetatauCMT/users/templatetags/custom_tags.py
+++ thetatauCMT/users/templatetags/custom_tags.py
@@ -7,8 +7,9 @@ register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def user_alter_form(context):
-    user = context['request'].user
-    if not user.is_anonymous and user.is_national_officer_group():
-        return UserAlterForm(data={'chapter': user.current_chapter.pk})
-    else:
-        return None
+    request = context.get('request', None)
+    if request:
+        user = context['request'].user
+        if not user.is_anonymous and user.is_national_officer_group():
+            return UserAlterForm(data={'chapter': user.current_chapter.pk})
+    return None
