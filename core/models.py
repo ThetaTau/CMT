e22d32b2af94c994c6da7a6e71fb256e28355959
Franklin Ventura DEV-Laptop <venturafranklin@gmail.com>
2018-09-03 21:38:36 -0700

    Use default time instead of auto time now for created

--- core/models.py
+++ core/models.py
@@ -91,7 +91,7 @@ class TimeStampedModel(models.Model):
     An abstract base class model that provides self-
     updating ``created`` and ``modified`` fields.
     """
-    created = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(default=timezone.now)
     modified = models.DateTimeField(auto_now=True)
 
     class Meta:
