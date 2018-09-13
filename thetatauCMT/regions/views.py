000285b45e865366c24f911d4aa5475873de9acd
Franklin Ventura DEV-Laptop <venturafranklin@gmail.com>
2018-09-12 18:06:27 -0700

    Set the default filter for region officer to R, VR, T, CS, S; Closes #46

--- thetatauCMT/regions/views.py
+++ thetatauCMT/regions/views.py
@@ -39,6 +39,11 @@ class RegionOfficerView(NatOfficerRequiredMixin,
         request_get = self.request.GET.copy()
         if cancel:
             request_get = QueryDict()
+        if not request_get:
+            ####### WARNING: this is a hack to enable initial filter field selection in django-filters########
+            # Create a mutable QueryDict object, default is immutable
+            request_get = QueryDict(mutable=True)
+            request_get.setlist("role", ['corresponding secretary', 'regent', 'scribe', 'treasurer', 'vice regent'])
         self.filter = self.filter_class(request_get,
                                         queryset=all_chapter_officers)
         self.filter.form.helper = self.formhelper_class()
