70005b42ed34e28823b4d0e17c248227e4b2c499
Franklin Ventura DEV-Laptop <venturafranklin@gmail.com>
2018-09-13 06:58:03 -0700

    Officer only required for chapter views

--- thetatauCMT/chapters/views.py
+++ thetatauCMT/chapters/views.py
@@ -1,8 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse
 from django.views.generic import DetailView, ListView, RedirectView
-from braces.views import GroupRequiredMixin
-from core.views import RequestConfig, OfficerMixin
+from core.views import RequestConfig, OfficerMixin, OfficerRequiredMixin
 from .models import Chapter
 from users.tables import UserTable
 
@@ -30,9 +29,9 @@ class ChapterRedirectView(LoginRequiredMixin, RedirectView):
                        kwargs={'username': self.request.user.username})
 
 
-class ChapterListView(LoginRequiredMixin, GroupRequiredMixin, OfficerMixin, ListView):
+class ChapterListView(OfficerRequiredMixin,
+                      LoginRequiredMixin, OfficerMixin, ListView):
     model = Chapter
     # These next two lines tell the view to index lookups by username
     slug_field = 'slug'
     slug_url_kwarg = 'slug'
-    group_required = u"natoff"
