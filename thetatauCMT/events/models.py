7a2c25f2de75b468fe2f921f4b2367f2a1ab0f5a
Franklin Ventura DEV-Laptop <venturafranklin@gmail.com>
2018-09-13 18:36:15 -0700

    Remove permalink

--- thetatauCMT/events/models.py
+++ thetatauCMT/events/models.py
@@ -41,7 +41,6 @@ class Event(TimeStampedModel):
     def __str__(self):
         return f"{self.name} on {self.date}"
 
-    @models.permalink
     def get_absolute_url(self):
         return (
             'events:detail', (), {
