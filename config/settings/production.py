1f3196758d61736b46608d059cf64ff5323980f0
Franklin Ventura <venturafranklin@gmail.com>
2018-11-18 14:57:19 -0700

    Set default email to CMT email

--- config/settings/production.py
+++ config/settings/production.py
@@ -110,7 +110,7 @@ TEMPLATES[0]['OPTIONS']['loaders'] = [  # noqa F405
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env(
     'DJANGO_DEFAULT_FROM_EMAIL',
-    default='Theta Tau Chapter Management Tool <noreply@thetatau.org>'
+    default='Theta Tau Chapter Management Tool <cmt@thetatau.org>'
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env('DJANGO_SERVER_EMAIL', default=DEFAULT_FROM_EMAIL)
