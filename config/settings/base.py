fac48e03f6011098d43f1f85796cb40fa93c8007
Franklin Ventura <venturafranklin@gmail.com>
2018-11-18 14:56:53 -0700

    Set default database with development info

--- config/settings/base.py
+++ config/settings/base.py
@@ -39,7 +39,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
 DATABASES = {
-    'default': env.db('DATABASE_URL', default='postgres://localhost/thetatauCMT'),
+    'default': env.db('DATABASE_URL', default='postgres://postgres:test@localhost:5432/thetatauCMT'),
 }
 DATABASES['default']['ATOMIC_REQUESTS'] = True
 
