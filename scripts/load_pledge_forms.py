238ee0cc62c1b6b41443e150d78760fc7448400b
Franklin Ventura <venturafranklin@gmail.com>
2018-12-09 21:55:27 -0700

    Add script to load old pledge forms from CSV files

---
+++ scripts/load_pledge_forms.py
@@ -0,0 +1,33 @@
+import csv
+import warnings
+from datetime import datetime
+from django.db import transaction
+from django.db.utils import IntegrityError
+from forms.models import PledgeForm
+from chapters.models import Chapter
+
+
+def run(*args):
+    """
+    Space seperated list with --script-args "path_to_file" "path2_to_file"
+    :param args: List of files to read pledge forms
+    :return: None
+    """
+    for path in args:
+        with open(path) as f:
+            reader = csv.DictReader(f)
+            for pledge_form in reader:
+                chapter_obj = Chapter.get_school_chapter(pledge_form['School Name'])
+                name = f'{pledge_form["Legal First Name"]} {pledge_form["Full Middle Name"]} {pledge_form["Last Name"]}'
+                form_obj = PledgeForm(
+                    name=name,
+                    chapter=chapter_obj,
+                    email=pledge_form['School E-mail'],
+                    created=datetime.strptime(pledge_form['Submission Date'], "%Y-%m-%d %H:%M:%S"),
+                )
+                try:
+                    with transaction.atomic():
+                        form_obj.save()
+                except IntegrityError as e:
+                    warnings.warn(f"{name} already existed for {chapter_obj}")
+                    continue
