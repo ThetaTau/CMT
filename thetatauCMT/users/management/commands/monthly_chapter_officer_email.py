b97957c78e3c67107f39395779d2ef2ec38e5718
Franklin Ventura <venturafranklin@gmail.com>
2019-02-24 10:14:49 -0700

    Add command to send monthly officer email

---
+++ thetatauCMT/users/management/commands/monthly_chapter_officer_email.py
@@ -0,0 +1,35 @@
+import datetime
+from django.core.management import BaseCommand
+from django.core.mail import send_mail
+from users.notifications import OfficerMonthly
+from chapters.models import Chapter
+
+
+class Command(BaseCommand):
+    # Show this when the user types help
+    help = "Send monthly email to chapter officers on the first of the month"
+
+    # A command must define handle()
+    def handle(self, *args, **options):
+        """
+        Pythonanywhere does not support use of celery.
+        Best way to trigger is to use daily scheduled task from Pythonanywhere
+        and check if the day is first of month.
+        :param args:
+        :param options:
+        :return:
+        """
+        today = datetime.date.today().day
+        if today == 1:
+            change_messages = []
+            for chapter in Chapter.objects.all():
+                result = OfficerMonthly(chapter).send()
+                change_messages.append(f"{result}: {chapter}")
+            change_message = '\n'.join(change_messages)
+            send_mail(f'CMT Monthly Email Task',
+                      f'Email sent to chapters:\n{change_message}',
+                      'cmt@thetatau.org',
+                      ['cmt@thetatau.org'],
+                      fail_silently=True)
+        else:
+            print("Not Today")
