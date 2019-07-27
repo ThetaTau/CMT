c73acb73577f2b40583e1af7a9330386a7f70b0f
Franklin Ventura <venturafranklin@gmail.com>
2019-07-27 11:35:01 -0700

    Remove file path from submissions table list

--- thetatauCMT/submissions/tables.py
+++ thetatauCMT/submissions/tables.py
@@ -1,31 +1,16 @@
 import django_tables2 as tables
 from django_tables2.utils import A
-from googleapiclient.http import HttpError
 from .models import Submission
-
-
-class FileURLColumn(tables.LinkColumn):
-    def compose_url(self, record, *args, **kwargs):
-        out_url = ""
-        try:
-            out_url = record.file.url
-        except TimeoutError:
-            pass  # Really want to remove all file uploads, ignore for now
-        except HttpError:
-            pass  # Really want to remove all file uploads, ignore for now
-        except BrokenPipeError:
-            pass
-        return out_url
 
 
 class SubmissionTable(tables.Table):
     name = tables.LinkColumn('submissions:update',
                              args=[A('pk')])
-    file = FileURLColumn()
 
     class Meta:
         model = Submission
         fields = ('name', 'date',
-                  'type', 'score', 'file')
+                  'type', 'score',
+                  )
         attrs = {"class": "table-striped table-bordered"}
         empty_text = "There are no submissions matching the search criteria..."
