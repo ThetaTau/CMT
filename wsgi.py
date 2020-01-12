54e487cce24fe28f3bea16e6c32e456cd65e8147
Franklin Ventura <venturafranklin@gmail.com>
2020-01-12 15:13:29 -0700

    Add maintenance file and command and wsgi config

---
+++ wsgi.py
@@ -0,0 +1,37 @@
+import os
+import sys
+path = ''
+if path not in sys.path:
+    sys.path.append(path)
+
+
+os.environ['DJANGO_SETTINGS_MODULE'] = ''
+os.environ['DJANGO_SECRET_KEY'] = ''
+os.environ['DJANGO_ALLOWED_HOSTS'] = ''
+os.environ['DJANGO_ADMIN_URL'] = ''
+os.environ['MAILGUN_API_KEY'] = ''
+os.environ['SENDGRID_API_KEY'] = ''
+os.environ['MAILGUN_DOMAIN'] = ''
+os.environ['DJANGO_AWS_ACCESS_KEY_ID'] = ''
+os.environ['DJANGO_AWS_SECRET_ACCESS_KEY'] = ''
+os.environ['DJANGO_AWS_STORAGE_BUCKET_NAME'] = ''
+os.environ['ROLLBAR_ACCESS'] = ''
+os.environ['DATABASE_URL_MYSQL'] = ''
+os.environ['DATABASE_URL'] = ''
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = ""
+
+
+from django.core.wsgi import get_wsgi_application
+
+MAINTENANCE_FILE = rf"{path}/maintenance_active"
+
+if not os.path.exists(MAINTENANCE_FILE):
+    application = get_wsgi_application()
+else:
+    def application(environ, start_response):
+        status = '503 Service Unavailable'
+        maintenance_file_path = rf"{path}/thetatauCMT/templates/503.html"
+        content = open(maintenance_file_path, 'r').read().encode('utf8')
+        response_headers = [('Content-Type', 'text/html'), ('Content-Length', str(len(content)))]
+        start_response(status, response_headers)
+        yield content.encode('utf8')
