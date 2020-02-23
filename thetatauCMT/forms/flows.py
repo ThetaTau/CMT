c15c1d52aba730b931b7f4f985aa767e6de49bba
Franklin Ventura <venturafranklin@gmail.com>
2020-02-23 15:00:28 -0700

    Add status changes to prealumn process

--- thetatauCMT/forms/flows.py
+++ thetatauCMT/forms/flows.py
@@ -1,10 +1,12 @@
+import datetime
 from viewflow import flow, frontend
 from viewflow.base import this, Flow
 from viewflow.compat import _
 from viewflow.flow import views as flow_views
+from core.models import forever
 from .models import PrematureAlumnus
 from .views import PrematureAlumnusCreateView
-from users.models import User
+from users.models import User, UserStatusChange
 
 
 @frontend.register
@@ -12,6 +14,7 @@ class PrematureAlumnusFlow(Flow):
     process_class = PrematureAlumnus
     process_title = _('Premature Alumnus Process')
     process_description = _('This process is for premature alumnus processing.')
+
     start = (
         flow.Start(
             PrematureAlumnusCreateView,
@@ -20,56 +23,19 @@ class PrematureAlumnusFlow(Flow):
         .Next(this.pending_status)
     )
 
-    start_officer = (
-        flow.Start(
-            flow_views.CreateProcessView,
-            fields=['form', 'user'],
-            task_title=_('Request Premature Alumnus Other Member'))
-        .Permission(auto_create=True)
-        .Next(this.exec_approve)
-    )
-
     pending_status = (
         flow.Handler(
-            this.set_status_assign_email,
-            task_title=_('Set pending status, send email, and assign officers'),
+            this.set_status_email,
+            task_title=_('Set pending status, send email.'),
         )
-        .Next(this.officer_assign)
+        .Next(this.exec_approve)
     )
-
-    officer_assign = (
-        flow.Split()
-        .Next(this.officer1_approve)
-        .Next(this.officer2_approve)
-    )
-
-    officer1_approve = (
-        flow.View(
-            flow_views.UpdateProcessView, fields=['officer1_ack', 'officer1_comments'],
-            task_title=_('Chapter Officer 1 Review'),
-            task_description=_("Acknowledgement required for {{ process.user }} Premature Alumnus request"),
-            task_result_summary=_("Request was {{ process.officer1_ack|yesno:'Acknowledged,Rejected' }}"))
-        .Assign(lambda act: act.process.officers.first())
-        .Next(this.officer_review_complete)
-    )
-
-    officer2_approve = (
-        flow.View(
-            flow_views.UpdateProcessView, fields=['officer2_ack', 'officer2_comments'],
-            task_title=_('Chapter Officer 2 Review'),
-            task_description=_("Acknowledgement required for {{ process.user }} Premature Alumnus request"),
-            task_result_summary=_("Request was {{ process.officer2_ack|yesno:'Acknowledged,Rejected' }}"))
-        .Assign(lambda act: act.process.officers.last())
-        .Next(this.officer_review_complete)
-    )
-
-    officer_review_complete = flow.Join().Next(this.exec_approve)
 
     exec_approve = (
         flow.View(
             flow_views.UpdateProcessView, fields=['approved_exec', 'exec_comments'],
             task_title=_('Executive Director Review'),
-            task_description=_("Pre Alumn review for {{ process.user }}"),
+            task_description=_("Pre Alumn Executive Director Review"),
             task_result_summary=_("Messsage was {{ process.approved_exec|yesno:'Approved,Rejected' }}"))
         .Assign(lambda act: User.objects.get(username="venturafranklin@gmail.com"))
         .Next(this.check_approve)
@@ -81,7 +47,7 @@ class PrematureAlumnusFlow(Flow):
             task_title=_('Premature Alumnus Approvement check'),
         )
         .Then(this.alumni_status)
-        .Else(this.send)
+        .Else(this.pending_undo)
     )
 
     alumni_status = (
@@ -92,42 +58,108 @@ class PrematureAlumnusFlow(Flow):
         .Next(this.send)
     )
 
+    pending_undo = (
+        flow.Handler(
+            this.pending_undo,
+            task_title=_('Set status active'),
+        )
+        .Next(this.send)
+    )
+
     send = (
         flow.Handler(
             this.send_approval_complete,
-            task_title=_('Send approval complete message'),
+            task_title=_('Send request complete message'),
         )
         .Next(this.complete)
     )
 
     complete = flow.End(
         task_title=_('Complete'),
+        task_result_summary=_("Request was {{ process.approved_exec|yesno:'Approved,Rejected' }}")
     )
 
-    def set_status_assign_email(self, activation):
+    def set_status_email(self, activation):
         """
         Need to set the pending status
         Email the user the form was received
-        Assign officers to review
         :param activation:
         :return:
         """
-        user = activation.process.created_by
-        chapter = activation.process.created_by.chapter
-        regent = chapter.get_current_officers_council(combine=False)[0].filter(roles__role="regent").first()
-        vice_regent = chapter.get_current_officers_council(combine=False)[0].filter(roles__role="vice regent").first()
-        scribe = chapter.get_current_officers_council(combine=False)[0].filter(roles__role="scribe").first()
-        if user != regent:
-            activation.process.officers.add(regent)
+        user = activation.process.user
+        created = activation.process.created
+        active = user.status.order_by('-end').filter(status='active').first()
+        if not active:
+            print(f"There was no active status for user {self.user}")
+            UserStatusChange(
+                user=user,
+                status='active',
+                created=created,
+                start=created - datetime.timedelta(days=365),
+                end=created,
+            ).save()
         else:
-            activation.process.officers.add(scribe)
-        if user != vice_regent:
-            activation.process.officers.add(vice_regent)
+            active.end = created
+            active.created = created
+            active.save()
+        alumnipends = user.status.filter(status='alumnipend')
+        if alumnipends:
+            alumnipend = alumnipends[0]
+            alumnipend.start = created
+            alumnipend.end = forever()
+            alumnipend.created = created
+            alumnipend.save()
+            for alumnipend in alumnipends[1:]:
+                alumnipend.delete()
         else:
-            activation.process.officers.add(scribe)
+            UserStatusChange(
+                user=user,
+                created=created,
+                status='alumnipend',
+                start=created,
+                end=forever(),
+            ).save()
+
+    def pending_undo(self, activation):
+        user = activation.process.user
+        alumnipends = user.status.filter(status='alumnipend')
+        if alumnipends:
+            for alumnipend in alumnipends:
+                alumnipend.delete()
 
     def set_alumni_status(self, activation):
-        print(activation.process.created_by)
+        user = activation.process.user
+        alumnipend = user.status.order_by('-end').filter(status='alumnipend').first()
+        created = activation.task.created
+        if not alumnipend:
+            print(f"There was no alumnipend status for user {self.user}")
+            UserStatusChange(
+                user=user,
+                status='alumnipend',
+                created=created,
+                start=created - datetime.timedelta(days=365),
+                end=created,
+            ).save()
+        else:
+            alumnipend.end = created
+            alumnipend.save()
+        alumnis = user.status.filter(status='alumni')
+        if alumnis:
+            alumni = alumnis[0]
+            alumni.start = created
+            alumni.end = forever()
+            alumni.created = created
+            alumni.save()
+            for alumni in alumnis[1:]:
+                alumni.delete()
+        else:
+            UserStatusChange(
+                user=user,
+                created=created,
+                status='alumni',
+                start=created,
+                end=forever(),
+            ).save()
 
     def send_approval_complete(self, activation):
         print(activation.process.created_by)
