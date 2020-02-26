9fe49dbbafcb9b0500820209c2add78aed678baf
Franklin Ventura <venturafranklin@gmail.com>
2020-02-25 20:59:25 -0700

    Set file storage working locally to local

--- config/settings/local_ventura.py
+++ config/settings/local_ventura.py
@@ -1,3 +1,3 @@
 from .local import *
-
+DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
 # GOOGLE_DRIVE_STORAGE_JSON_KEY_FILE = r"E:\workspace\CMT\thetatauCMT\secrets\ChapterManagementTool-b239bceff1a7.json"
