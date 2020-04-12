e0ca9e8e0b8935d657e60850ddcdb2a3110c8ec8
Franklin Ventura <venturafranklin@gmail.com>
2020-04-12 10:44:45 -0700

    Add django email live in base config

--- config/settings/base.py
+++ config/settings/base.py
@@ -241,9 +241,13 @@ FIXTURE_DIRS = (
 # EMAIL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
-# This does not work currently in dev
-EMAIL_BACKEND = env('DJANGO_EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
-
+DJANGO_EMAIL_LIVE = env.bool('DJANGO_EMAIL_LIVE', True)
+if DJANGO_EMAIL_LIVE:
+    EMAIL_BACKEND = env('DJANGO_EMAIL_BACKEND',
+                        default='django.core.mail.backends.console.EmailBackend')
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = str(ROOT_DIR('email_tests'))
 # ADMIN
 # ------------------------------------------------------------------------------
 # Django Admin URL regex.
