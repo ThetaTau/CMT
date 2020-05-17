02356e304cbb0a2661f4f0a362189321aaaf4e56
Franklin Ventura <venturafranklin@gmail.com>
2020-05-17 10:31:31 -0700

    Cleanup and squash all migrations

--- thetatauCMT/contrib/sites/migrations/0003_set_site_domain_and_name.py
+++ thetatauCMT/contrib/sites/migrations/0003_set_site_domain_and_name.py
@@ -9,32 +9,28 @@ from django.db import migrations
 
 def update_site_forward(apps, schema_editor):
     """Set site domain and name."""
-    Site = apps.get_model('sites', 'Site')
+    Site = apps.get_model("sites", "Site")
     Site.objects.update_or_create(
         id=settings.SITE_ID,
         defaults={
-            'domain': 'cmt.thetatau.org',
-            'name': 'Theta Tau Chapter Management Tool'
-        }
+            "domain": "cmt.thetatau.org",
+            "name": "Theta Tau Chapter Management Tool",
+        },
     )
 
 
 def update_site_backward(apps, schema_editor):
     """Revert site domain and name to default."""
-    Site = apps.get_model('sites', 'Site')
+    Site = apps.get_model("sites", "Site")
     Site.objects.update_or_create(
-        id=settings.SITE_ID,
-        defaults={
-            'domain': 'example.com',
-            'name': 'example.com'
-        }
+        id=settings.SITE_ID, defaults={"domain": "example.com", "name": "example.com"}
     )
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('sites', '0002_alter_domain_unique'),
+        ("sites", "0002_alter_domain_unique"),
     ]
 
     operations = [
