1a58396eabf7b2fab6a68fe4e67c685b887db7c3
Franklin Ventura <venturafranklin@gmail.com>
2020-05-24 13:46:13 -0700

    Add events factory

---
+++ thetatauCMT/events/tests/factories.py
@@ -0,0 +1,26 @@
+from datetime import date
+import factory
+from scores.models import ScoreType
+from chapters.tests.factories import ChapterFactory
+from ..models import Event
+
+
+class EventFactory(factory.django.DjangoModelFactory):
+    created = factory.Faker("date_time_between", start_date="-1y", end_date="+1y")
+    modified = factory.Faker("date_time_between", start_date="-1y", end_date="+1y")
+    name = factory.Faker("sentence", nb_words=3)
+    date = factory.Faker("date_between", start_date="-4y", end_date="+4y")
+    type = factory.Iterator(ScoreType.objects.filter(type="Evt"))
+    chapter = factory.SubFactory(ChapterFactory)
+    description = factory.Faker("paragraph", nb_sentences=2)
+    members = factory.Faker("random_int", max=200)
+    alumni = factory.Faker("random_int", max=200)
+    pledges = factory.Faker("random_int", max=50)
+    guests = factory.Faker("random_int", max=200)
+    duration = factory.Faker("random_int", max=200)
+    stem = factory.Faker("boolean")
+    host = factory.Faker("boolean")
+    miles = factory.Faker("random_int", max=5000)
+
+    class Meta:
+        model = Event
