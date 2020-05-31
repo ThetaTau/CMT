7960a879d7dd3cf7bfbb486f455baf4e81c2c04c
Franklin Ventura <venturafranklin@gmail.com>
2020-05-31 12:05:37 -0700

    Use pytest factoryboy for chapter and regions

--- thetatauCMT/conftest.py
+++ thetatauCMT/conftest.py
@@ -1,6 +1,8 @@
 import pytest
-
+from pytest_factoryboy import register
 from django.core.management import call_command
+from chapters.tests.factories import ChapterFactory, ChapterCurriculaFactory
+from regions.tests.factories import RegionFactory
 
 
 @pytest.fixture(scope="session")
@@ -8,3 +10,8 @@ def django_db_setup(django_db_setup, django_db_blocker):
     with django_db_blocker.unblock():
         call_command("loaddata", "scoretypes.json")
         call_command("loaddata", "tasks.json")
+
+
+register(RegionFactory)
+register(ChapterFactory)
+register(ChapterCurriculaFactory)
