fcb62e0ef8e98a61c34732fe60dc913a5bc5da5b
Franklin Ventura <venturafranklin@gmail.com>
2020-05-31 12:03:47 -0700

    Reset all users tests

--- thetatauCMT/users/tests/test_models.py
+++ thetatauCMT/users/tests/test_models.py
@@ -1,19 +1,13 @@
 from test_plus.test import TestCase
 
 
-class TestUser(TestCase):
+def test_get_absolute_url(tp):
+    expected_url = "/users/testuser/"
+    user = tp.make_user()
+    reversed_url = user.get_absolute_url()
+    assert expected_url == reversed_url
 
-    def setUp(self):
-        self.user = self.make_user()
 
-    def test__str__(self):
-        self.assertEqual(
-            self.user.__str__(),
-            'testuser'  # This is the default username for self.make_user()
-        )
-
-    def test_get_absolute_url(self):
-        self.assertEqual(
-            self.user.get_absolute_url(),
-            '/users/testuser/'
-        )
+def test__str__(tp):
+    user = tp.make_user()
+    assert "testuser" == user.__str__()
