b2f362d1b5b22c409dfac8c0edf49f055eb47125
Franklin Ventura <venturafranklin@gmail.com>
2020-05-31 21:22:30 -0700

    Add auto login user and password for django testing

--- thetatauCMT/conftest.py
+++ thetatauCMT/conftest.py
@@ -47,6 +47,22 @@ def django_db_setup(django_db_setup, django_db_blocker):
         )
 
 
+@pytest.fixture
+def test_password():
+    return "strong-test-pass"
+
+
+@pytest.fixture
+def auto_login_user(db, client, user_factory, test_password):
+    def make_auto_login(user=None):
+        if user is None:
+            user = user_factory(password=test_password)
+        client.login(username=user.username, password=test_password)
+        return client, user
+
+    return make_auto_login
+
+
 register(RegionFactory)
 register(ChapterFactory)
 register(ChapterCurriculaFactory)
