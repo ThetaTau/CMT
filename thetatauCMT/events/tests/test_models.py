c393d91033592e281e55f6a6471a684de9a4bbec
Franklin Ventura <venturafranklin@gmail.com>
2020-05-31 20:15:27 -0700

    Add test for calculate_meeting_attendance

--- thetatauCMT/events/tests/test_models.py
+++ thetatauCMT/events/tests/test_models.py
@@ -1,8 +1,13 @@
 import datetime
 import pytest
+import factory
+from faker import Faker
 from django.utils.text import slugify
 from events.tests.factories import EventFactory
 from events.models import Event
+from scores.models import ScoreType
+
+fake = Faker()
 
 
 @pytest.mark.django_db
@@ -45,3 +50,45 @@ def test_chapter_events(chapter, event_factory):
     expected_events = event_factory.create_batch(10, chapter=chapter)
     actual_events = Event.chapter_events(chapter)
     assert set(list(actual_events)) == set(expected_events)
+
+
+@pytest.mark.django_db
+def test_calculate_meeting_attendance(
+    chapter, event_factory, user_status_change_factory
+):
+    score_type = ScoreType.objects.get(name="Attendance at meetings")
+    users = user_status_change_factory.create_batch(
+        20,
+        status="active",
+        user__chapter=chapter,
+        start=factory.Faker("date_between", start_date="-1y", end_date="today"),
+        end=factory.Faker("date_between", start_date="today", end_date="+1y"),
+    )
+    events = event_factory.create_batch(
+        10,
+        calculate_score=False,
+        type=score_type,
+        members=5,
+        chapter=chapter,
+        date=factory.Faker("date_between", start_date="-15d", end_date="-5d"),
+    )
+    date = fake.date_between(start_date="-15d", end_date="-5d")
+    actual_score = Event.calculate_meeting_attendance(chapter, date)
+    # 10 events at 25% attendance each eval "15*MEETINGS"
+    # Total score is 3.75 and individual event is 3.75/10=0.38
+    assert actual_score == 0.39
+
+
+@pytest.mark.django_db
+def test_calculate_meeting_attendance_no_events(chapter, user_status_change_factory):
+    users = user_status_change_factory.create_batch(
+        20,
+        status="active",
+        user__chapter=chapter,
+        start=factory.Faker("date_between", start_date="-1y", end_date="today"),
+        end=factory.Faker("date_between", start_date="today", end_date="+1y"),
+    )
+    date = fake.date_between(start_date="-15d", end_date="-5d")
+    actual_score = Event.calculate_meeting_attendance(chapter, date)
+    # No events, should be 0 score
+    assert actual_score == 0
