a5b72fc8d9718472f39c7ce61ebe3302fbddc389
Franklin Ventura <venturafranklin@gmail.com>
2020-05-31 19:10:27 -0700

    Regsiter all factories

--- thetatauCMT/conftest.py
+++ thetatauCMT/conftest.py
@@ -1,9 +1,23 @@
 import pytest
 from pytest_factoryboy import register
 from django.core.management import call_command
+from ballots.tests.factories import BallotFactory, BallotCompleteFactory
 from chapters.tests.factories import ChapterFactory, ChapterCurriculaFactory
-from regions.tests.factories import RegionFactory
 from events.tests.factories import EventFactory
+from finances.tests.factories import TransactionFactory
+from regions.tests.factories import RegionFactory
+from scores.tests.factories import ScoreChapterFactory
+from submissions.tests.factories import SubmissionFactory
+from tasks.tests.factories import TaskChapterFactory
+from users.tests.factories import (
+    UserFactory,
+    UserAlterFactory,
+    UserOrgParticipateFactory,
+    UserSemesterGPAFactory,
+    UserSemesterServiceHoursFactory,
+    UserRoleChangeFactory,
+    UserStatusChangeFactory,
+)
 
 
 @pytest.fixture(scope="session")
@@ -17,3 +31,16 @@ register(RegionFactory)
 register(ChapterFactory)
 register(ChapterCurriculaFactory)
 register(EventFactory)
+register(BallotFactory)
+register(BallotCompleteFactory)
+register(TransactionFactory)
+register(ScoreChapterFactory)
+register(SubmissionFactory)
+register(TaskChapterFactory)
+register(UserFactory)
+register(UserAlterFactory)
+register(UserOrgParticipateFactory)
+register(UserSemesterGPAFactory)
+register(UserSemesterServiceHoursFactory)
+register(UserRoleChangeFactory)
+register(UserStatusChangeFactory)
