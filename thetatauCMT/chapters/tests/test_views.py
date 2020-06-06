7cfc94fd6da9c6ca90c437a7a70d6a528fd5bbb0
Franklin Ventura <venturafranklin@gmail.com>
2020-06-06 11:58:23 -0700

    Add tests of chapter list view

--- thetatauCMT/chapters/tests/test_views.py
+++ thetatauCMT/chapters/tests/test_views.py
@@ -19,3 +19,30 @@ def test_chapter_detail_view(auto_login_user):
     assert f"{chapter.name} in the {chapter.region} Region" in response.content.decode(
         "UTF-8"
     )
+
+
+def test_chapter_list_view_denied(auto_login_user):
+    client, user = auto_login_user()
+    url = reverse("chapters:list")
+    response_no_redirect = client.get(url)
+    assert response_no_redirect.status_code == 302
+    assert response_no_redirect.url == r"/"
+    response = client.get(url, follow=True)
+    assert response.status_code == 200
+    assert f"Only officers can edit this" in response.content.decode("UTF-8")
+
+
+def test_chapter_list_view_chapter_officer(auto_login_user):
+    client, user = auto_login_user(make_officer="chapter")
+    url = reverse("chapters:list")
+    response = client.get(url, follow=True)
+    assert response.status_code == 200
+    assert f"Filter Chapters" in response.content.decode("UTF-8")
+
+
+def test_chapter_list_view_natoff(auto_login_user):
+    client, user = auto_login_user(make_officer="national")
+    url = reverse("chapters:list")
+    response = client.get(url, follow=True)
+    assert response.status_code == 200
+    assert f"Filter Chapters" in response.content.decode("UTF-8")
