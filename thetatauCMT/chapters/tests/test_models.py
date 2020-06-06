29298d6ddc9963cada48d2dda3cbfab8a20c79d9
Franklin Ventura <venturafranklin@gmail.com>
2020-06-06 14:39:24 -0700

    Add tests for chapter test_get_current_officers_council

--- thetatauCMT/chapters/tests/test_models.py
+++ thetatauCMT/chapters/tests/test_models.py
@@ -1,4 +1,5 @@
 import pytest
+from pytest_django.asserts import assertQuerysetEqual
 from chapters.tests.factories import ChapterFactory, ChapterCurriculaFactory
 from chapters.models import Chapter, ChapterCurricula
 
@@ -73,3 +74,40 @@ def test_next_advisor_number(chapter, user_factory):
     user_factory.reset_sequence(7000)
     user_factory.create_batch(10, chapter=chapter)
     assert chapter.next_advisor_number == 7011
+
+
+@pytest.mark.django_db
+def test_get_current_officers_council_previous(chapter, user_factory):
+    result = chapter.get_current_officers_council(combine=False)
+    assert result[1] is True
+    assert result[0].count() == 0
+    regent = user_factory.create(
+        chapter=chapter, make_officer="regent", make_officer__current=False
+    )
+    vice = user_factory.create(
+        chapter=chapter, make_officer="vice regent", make_officer__current=False
+    )
+    old_officer_pks = [regent.pk, vice.pk]
+    result = chapter.get_current_officers_council(combine=False)
+    assert result[1] is True
+    assertQuerysetEqual(result[0], old_officer_pks, lambda o: o.pk, ordered=False)
+
+
+@pytest.mark.django_db
+def test_get_current_officers_council(chapter, user_factory):
+    result = chapter.get_current_officers_council(combine=False)
+    assert result[1] is True
+    assert result[0].count() == 0
+    regent = user_factory.create(chapter=chapter, make_officer="regent")
+    vice = user_factory.create(chapter=chapter, make_officer="vice regent")
+    treasurer = user_factory.create(chapter=chapter, make_officer="treasurer")
+    scribe = user_factory.create(chapter=chapter, make_officer="scribe")
+    corsec = user_factory.create(
+        chapter=chapter, make_officer="corresponding secretary"
+    )
+    officer_pks = [regent.pk, vice.pk, treasurer.pk, scribe.pk, corsec.pk]
+    result = chapter.get_current_officers_council(combine=False)
+    assert result[1] is False
+    assertQuerysetEqual(result[0], officer_pks, lambda o: o.pk, ordered=False)
+    result = chapter.get_current_officers_council_specific()
+    assert (regent, scribe, vice, treasurer) == result
