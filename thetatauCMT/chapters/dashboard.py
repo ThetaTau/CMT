2fa05fcf450980b9c660fda4d35c0451631ca123
Anh Mac <anhmvc@gmail.com>
2020-08-20 21:57:21 -0700

    Added Stats section, cleaned up graphs

--- thetatauCMT/chapters/dashboard.py
+++ thetatauCMT/chapters/dashboard.py
@@ -26,11 +26,11 @@ from chapters.models import Chapter
 from users.models import User, UserStatusChange
 
 colors = {
-    "Actives": "darkred",
-    "Inactives": "grey",
-    "Pledges": "green",
-    "Depledges": "firebrick",
-    "Alumnis": "black",
+    "Actives": "#ff9f43",
+    "Inactives": "#a29bfe",
+    "Pledges": "#1dd1a1",
+    "Depledges": "#d63031",
+    "Alumnis": "#2e86de",
     "Fall": "#EB8686",
     "Spring": "#E66868",
 }
@@ -54,6 +54,13 @@ def get_group(g, key):
         return 0
 
 
+def calculateChange(initial, final):
+    try:
+        return round(((final - initial) / initial * 100), 2)
+    except ZeroDivisionError:
+        return 0
+
+
 ## -------------------------------------------------------------------------------
 
 app.layout = html.Div(
@@ -69,105 +76,89 @@ app.layout = html.Div(
         ),
         html.Div(
             children=[
+                html.P("Select date range:"),
+                dcc.RangeSlider(
+                    id="years-slider",
+                    min=2010,
+                    max=2021,
+                    marks={
+                        2010: "2010",
+                        2012: "2012",
+                        2014: "2014",
+                        2016: "2016",
+                        2018: "2018",
+                        2020: "2020",
+                    },
+                    value=[2014, 2020],
+                ),
+            ],
+            style=dict(
+                borderRadius=5,
+                backgroundColor="#f9f9f9",
+                margin=5,
+                padding=30,
+                position="relative",
+                boxShadow="2px 2px 2px lightgrey",
+            ),
+        ),
+        html.Div(
+            children=[
                 html.Div(
-                    children=[
-                        html.P("Select semester:"),
-                        dcc.Dropdown(
-                            id="semester-dropdown",
-                            options=[
-                                {"label": "Fall", "value": 1},
-                                {"label": "Spring", "value": 2},
-                            ],
-                            value=1,
-                        ),
-                        html.Div(
-                            children=[
-                                html.P("Select date range:", style=dict(marginTop=15),),
-                                dcc.RangeSlider(
-                                    id="years-slider",
-                                    min=2010,
-                                    max=2021,
-                                    marks={
-                                        2010: "2010",
-                                        2012: "2012",
-                                        2014: "2014",
-                                        2016: "2016",
-                                        2018: "2018",
-                                        2020: "2020",
-                                    },
-                                    value=[2014, 2020],
-                                ),
-                            ],
-                            style=dict(padding=8, margin=0),
-                        ),
-                        html.Div(
-                            children=[
-                                html.H5(
-                                    "Chapter size breakdown for selected date range"
-                                )
-                            ],
-                            style=dict(marginTop=60, textAlign="center"),
-                        ),
-                        html.Div(
-                            children=[
-                                html.Div(
-                                    [html.H4(id="actives-num"), html.H6("Actives")],
-                                    id="actives",
-                                    style=dict(
-                                        borderRadius=5,
-                                        backgroundColor="#fcd781",
-                                        margin=10,
-                                        padding=15,
-                                        position="relative",
-                                        boxShadow="2px 2px 2px lightgrey",
-                                        display="flex",
-                                        flexDirection="column",
-                                        textAlign="center",
-                                    ),
-                                ),
-                                html.Div(
-                                    [html.H4(id="alumni-num"), html.H6("Alumnis"),],
-                                    id="alumni",
-                                    style=dict(
-                                        borderRadius=5,
-                                        backgroundColor="rgba(168,7,7,0.6)",
-                                        margin=10,
-                                        padding=15,
-                                        position="relative",
-                                        boxShadow="2px 2px 2px lightgrey",
-                                        display="flex",
-                                        flexDirection="column",
-                                        textAlign="center",
-                                    ),
-                                ),
-                            ],
-                            style=dict(
-                                display="flex",
-                                flexDirection="row",
-                                marginTop=10,
-                                marginLeft=50,
-                            ),
-                        ),
-                    ],
+                    children=[html.H6("Actives"), html.Div(id="actives-num")],
                     style=dict(
                         borderRadius=5,
                         backgroundColor="#f9f9f9",
                         margin=10,
                         padding=15,
-                        width="30.6666666667%",
+                        width="20%",
                         position="relative",
                         boxShadow="2px 2px 2px lightgrey",
                     ),
                 ),
-                # Graph 1: Chapter Size by Semester Over Time
                 html.Div(
-                    children=[dcc.Graph(id="chapter-size-graph"),],
+                    children=[html.H6("Inactives"), html.Div(id="inactives-num")],
                     style=dict(
                         borderRadius=5,
                         backgroundColor="#f9f9f9",
                         margin=10,
                         padding=15,
-                        width="65.3333333333%",
+                        width="20%",
+                        position="relative",
+                        boxShadow="2px 2px 2px lightgrey",
+                    ),
+                ),
+                html.Div(
+                    children=[html.H6("Pledges"), html.Div(id="pledges-num")],
+                    style=dict(
+                        borderRadius=5,
+                        backgroundColor="#f9f9f9",
+                        margin=10,
+                        padding=15,
+                        width="20%",
+                        position="relative",
+                        boxShadow="2px 2px 2px lightgrey",
+                    ),
+                ),
+                html.Div(
+                    children=[html.H6("Depledges"), html.Div(id="depledges-num")],
+                    style=dict(
+                        borderRadius=5,
+                        backgroundColor="#f9f9f9",
+                        margin=10,
+                        padding=15,
+                        width="20%",
+                        position="relative",
+                        boxShadow="2px 2px 2px lightgrey",
+                    ),
+                ),
+                html.Div(
+                    children=[html.H6("Alumnis"), html.Div(id="alumni-num")],
+                    style=dict(
+                        borderRadius=5,
+                        backgroundColor="#f9f9f9",
+                        margin=10,
+                        padding=15,
+                        width="20%",
                         position="relative",
                         boxShadow="2px 2px 2px lightgrey",
                     ),
@@ -176,10 +167,21 @@ app.layout = html.Div(
             style=dict(display="flex", flexDirection="row"),
         ),
         html.Div(
+            children=[dcc.Graph(id="composition-graph"),],
+            style=dict(
+                borderRadius=5,
+                backgroundColor="#f9f9f9",
+                margin=10,
+                padding=15,
+                position="relative",
+                boxShadow="2px 2px 2px lightgrey",
+            ),
+        ),
+        html.Div(
             children=[
                 # Graph 2: Number of Actives/Inactives/Pledges Over Time
                 html.Div(
-                    children=[dcc.Graph(id="composition-graph"),],
+                    children=[dcc.Graph(id="chapter-size-graph"),],
                     style=dict(
                         borderRadius=5,
                         backgroundColor="#f9f9f9",
@@ -233,6 +235,8 @@ def members_graph(data, years, **kwargs):
         "Actives": [],
         "Inactives": [],
         "Pledges": [],
+        "Depledges": [],
+        "Alumnis": [],
     }
     YEARS = [str(year) for year in range(years[0], years[1] + 1)]
     df = pd.DataFrame.from_dict(data)
@@ -245,10 +249,18 @@ def members_graph(data, years, **kwargs):
         groups["Actives"].append(get_group(gb, ("active", True)))
         groups["Inactives"].append(get_group(gb, ("away", True)))
         groups["Pledges"].append(get_group(gb, ("prospective", True)))
+        groups["Depledges"].append(get_group(gb, ("depledge", True)))
+        groups["Alumnis"].append(get_group(gb, ("alumni", True)))
 
     TotalMembers = [
-        x + y + z
-        for x, y, z in zip(groups["Actives"], groups["Inactives"], groups["Pledges"])
+        j + k + x + y + z
+        for j, k, x, y, z in zip(
+            groups["Actives"],
+            groups["Inactives"],
+            groups["Pledges"],
+            groups["Depledges"],
+            groups["Alumnis"],
+        )
     ]
 
     for key, value in groups.items():
@@ -293,32 +305,42 @@ def members_graph(data, years, **kwargs):
         paper_bgcolor="#F9F9F9",
         updatemenus=[
             dict(
-                type="buttons",
                 direction="down",
-                active=0,
-                x=0.1,
-                y=1.0,
+                showactive=True,
+                y=2.0,
+                xanchor="left",
+                yanchor="top",
                 buttons=list(
                     [
                         dict(
                             label="All",
                             method="update",
-                            args=[{"visible": [True, True, True]}],
+                            args=[{"visible": [True, True, True, True, True]}],
                         ),
                         dict(
                             label="Actives",
                             method="update",
-                            args=[{"visible": [True, False, False]}],
+                            args=[{"visible": [True, False, False, False, False]}],
                         ),
                         dict(
                             label="Inactives",
                             method="update",
-                            args=[{"visible": [False, True, False]}],
+                            args=[{"visible": [False, True, False, False, False]}],
                         ),
                         dict(
                             label="Pledges",
                             method="update",
-                            args=[{"visible": [False, False, True]}],
+                            args=[{"visible": [False, False, True, False, False]}],
+                        ),
+                        dict(
+                            label="Depledges",
+                            method="update",
+                            args=[{"visible": [False, False, False, True, False]}],
+                        ),
+                        dict(
+                            label="Alumni",
+                            method="update",
+                            args=[{"visible": [False, False, False, False, True]}],
                         ),
                     ]
                 ),
@@ -393,10 +415,12 @@ def pledge_depledge_graph(data, years, **kwargs):
         updatemenus=[
             dict(
                 type="buttons",
-                direction="down",
+                direction="right",
                 active=0,
-                x=0.1,
-                y=1.0,
+                xanchor="center",
+                x=0.5,
+                y=-0.5,
+                yanchor="bottom",
                 buttons=list(
                     [
                         dict(
@@ -482,52 +506,123 @@ def chapter_size_graph(data, years, **kwargs):
 
 @app.callback(
     Output("actives-num", "children"),
-    [
-        Input("chapter-data", "data"),
-        Input("semester-dropdown", "value"),
-        Input("years-slider", "value"),
-    ],
+    [Input("chapter-data", "data"), Input("years-slider", "value"),],
 )
-def update_actives_num(data, semester, years_slider, **kwargs):
-    if semester == 1:
-        date_str = "-12-01"
-    if semester == 2:
-        date_str = "-05-01"
-
-    total = 0
+def actives_stats(data, years, **kwargs):
     df = pd.DataFrame.from_dict(data)
-    for i in range(years_slider[0], years_slider[1] + 1):
-        date = str(i) + date_str
-        df["term"] = (df["start"] <= date) & (df["end"] >= date)
-        gb = df.groupby(["status", "term"])
-        total += get_group(gb, ("active", True))
+    date_start = str(years[0]) + "-01-01"
+    date_end = str(years[1]) + "-01-01"
+    df["start_range"] = (df["start"] <= date_start) & (df["end"] >= date_start)
+    df["end_range"] = (df["start"] <= date_end) & (df["end"] >= date_end)
+    gb_start = df.groupby(["status", "start_range"])
+    gb_end = df.groupby(["status", "end_range"])
+    change = calculateChange(
+        get_group(gb_start, ("active", True)), get_group(gb_end, ("active", True))
+    )
 
-    return total
+    if change > 0:
+        return html.H2("+{}%".format(change), style=dict(color="#1dd1a1"))
+    if change < 0:
+        return html.H2("{}%".format(change), style=dict(color="#ff6b6b"))
+    else:
+        return html.H2("N/A", style=dict(color="#b2bec3"))
+
+
+@app.callback(
+    Output("inactives-num", "children"),
+    [Input("chapter-data", "data"), Input("years-slider", "value"),],
+)
+def actives_stats(data, years, **kwargs):
+    df = pd.DataFrame.from_dict(data)
+    date_start = str(years[0]) + "-01-01"
+    date_end = str(years[1]) + "-01-01"
+    df["start_range"] = (df["start"] <= date_start) & (df["end"] >= date_start)
+    df["end_range"] = (df["start"] <= date_end) & (df["end"] >= date_end)
+    gb_start = df.groupby(["status", "start_range"])
+    gb_end = df.groupby(["status", "end_range"])
+    change = calculateChange(
+        get_group(gb_start, ("away", True)), get_group(gb_end, ("away", True))
+    )
+
+    if change > 0:
+        return html.H2("+{}%".format(change), style=dict(color="#1dd1a1"))
+    if change < 0:
+        return html.H2("{}%".format(change), style=dict(color="#ff6b6b"))
+    else:
+        return html.H2("N/A", style=dict(color="#b2bec3"))
+
+
+@app.callback(
+    Output("pledges-num", "children"),
+    [Input("chapter-data", "data"), Input("years-slider", "value"),],
+)
+def pledges_stats(data, years, **kwargs):
+    df = pd.DataFrame.from_dict(data)
+    date_start = str(years[0]) + "-01-01"
+    date_end = str(years[1]) + "-01-01"
+    df["start_range"] = (df["start"] <= date_start) & (df["end"] >= date_start)
+    df["end_range"] = (df["start"] <= date_end) & (df["end"] >= date_end)
+    gb_start = df.groupby(["status", "start_range"])
+    gb_end = df.groupby(["status", "end_range"])
+    change = calculateChange(
+        get_group(gb_start, ("prospective", True)),
+        get_group(gb_end, ("prospective", True)),
+    )
+
+    if change > 0:
+        return html.H2("+{}%".format(change), style=dict(color="#1dd1a1"))
+    if change < 0:
+        return html.H2("{}%".format(change), style=dict(color="#ff6b6b"))
+    else:
+        return html.H2("N/A", style=dict(color="#b2bec3"))
+
+
+@app.callback(
+    Output("depledges-num", "children"),
+    [Input("chapter-data", "data"), Input("years-slider", "value"),],
+)
+def actives_stats(data, years, **kwargs):
+    df = pd.DataFrame.from_dict(data)
+    date_start = str(years[0]) + "-01-01"
+    date_end = str(years[1]) + "-01-01"
+    df["start_range"] = (df["start"] <= date_start) & (df["end"] >= date_start)
+    df["end_range"] = (df["start"] <= date_end) & (df["end"] >= date_end)
+    gb_start = df.groupby(["status", "start_range"])
+    gb_end = df.groupby(["status", "end_range"])
+    change = calculateChange(
+        get_group(gb_start, ("depledge", True)), get_group(gb_end, ("depledge", True))
+    )
+
+    if change > 0:
+        return html.H2("+{}%".format(change), style=dict(color="#1dd1a1"))
+    if change < 0:
+        return html.H2("{}%".format(change), style=dict(color="#ff6b6b"))
+    else:
+        return html.H2("N/A", style=dict(color="#b2bec3"))
 
 
 @app.callback(
     Output("alumni-num", "children"),
-    [
-        Input("chapter-data", "data"),
-        Input("semester-dropdown", "value"),
-        Input("years-slider", "value"),
-    ],
+    [Input("chapter-data", "data"), Input("years-slider", "value"),],
 )
-def update_alumni_num(data, semester, years_slider, **kwargs):
-    if semester == 1:
-        date_str = "-12-01"
-    if semester == 2:
-        date_str = "-05-01"
-
-    total = 0
+def alumni_stats(data, years, **kwargs):
     df = pd.DataFrame.from_dict(data)
-    for i in range(years_slider[0], years_slider[1] + 1):
-        date = str(i) + date_str
-        df["term"] = (df["start"] <= date) & (df["end"] >= date)
-        gb = df.groupby(["status", "term"])
-        total += get_group(gb, ("alumni", True))
+    date_start = str(years[0]) + "-01-01"
+    date_end = str(years[1]) + "-01-01"
+    df["start_range"] = (df["start"] <= date_start) & (df["end"] >= date_start)
+    df["end_range"] = (df["start"] <= date_end) & (df["end"] >= date_end)
+    gb_start = df.groupby(["status", "start_range"])
+    gb_end = df.groupby(["status", "end_range"])
+    change = calculateChange(
+        get_group(gb_start, ("alumni", True)), get_group(gb_end, ("alumni", True))
+    )
 
-    return total
+    if change > 0:
+        return html.H2("+{}%".format(change), style=dict(color="#1dd1a1"))
+    if change < 0:
+        return html.H2("{}%".format(change), style=dict(color="#ff6b6b"))
+    else:
+        return html.H2("N/A", style=dict(color="#b2bec3"))
 
 
 if __name__ == "__main__":
