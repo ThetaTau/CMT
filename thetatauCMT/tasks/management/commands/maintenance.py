13da9a06797626fda3064f9557ae7f734af1dec1
Franklin Ventura <franklin.ventura@roche.com>
2021-06-27 12:23:07 -0700

    Fix maintenance mode turn on

--- thetatauCMT/tasks/management/commands/maintenance.py
+++ thetatauCMT/tasks/management/commands/maintenance.py
@@ -5,7 +5,7 @@ from django.core.management import BaseCommand
 
 
 logger = logging.getLogger(__name__)
-MAINTENANCE_FILE = settings.ROOT_DIR + "maintenance_active"
+MAINTENANCE_FILE = settings.ROOT_DIR / "maintenance_active"
 
 
 # The class must be named Command, and subclass BaseCommand
@@ -23,11 +23,11 @@ class Command(BaseCommand):
         off = options.get("off", True)
         logger.info(f"Maintenance mode will be turned on: {on}; off: {off}")
         if on:
-            "create a file that will mean maintenance mode is active"
+            print("create a file that will mean maintenance mode is active")
             open(MAINTENANCE_FILE, "w").close()
             logger.info("Maintenance mode started")
         if off:
-            "remove maintenance mode file"
+            print("remove maintenance mode file")
             if os.path.isfile(MAINTENANCE_FILE):
                 os.remove(MAINTENANCE_FILE)
                 logger.info("Maintenance mode stop")
