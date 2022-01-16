45fdcd899cf96d2ffc44be8448697a6b126cbc30
Franklin Ventura <franklin.ventura@roche.com>
2022-01-16 12:14:45 -0700

    Add roll book download all button; Clean up init buttons; Working #508

--- thetatauCMT/forms/urls.py
+++ thetatauCMT/forms/urls.py
@@ -168,19 +168,4 @@ urlpatterns = [
         view=views.ResignationListView.as_view(),
         name="resign_list",
     ),
-    # url(
-    #     regex=r'^~status-change/$',
-    #     view=views.StatusChangeView.as_view(),
-    #     name='redirect'
-    # ),
-    # url(
-    #     regex=r'^~/$',
-    #     view=views.FormUpdateView.as_view(),
-    #     name='update'
-    # ),
-    # url(
-    #     regex=r'^(?P<year>d{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2})/(?P<slug>[-w]+)/$',
-    #     view=views.EventDetailView.as_view(),
-    #     name='detail'
-    # ),
 ]
