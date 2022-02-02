6b2edd07c2174b306b12df1067771f537d044195
Kevin Martinez <kom18d@my.fsu.edu>
2022-02-02 18:54:51 -0500

    Add picture form

--- thetatauCMT/events/forms.py
+++ thetatauCMT/events/forms.py
@@ -2,7 +2,7 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Fieldset, Row, Submit
 from crispy_forms.bootstrap import FormActions, InlineField, StrictButton
 from django import forms
-from .models import Event
+from .models import Event, Picture
 
 
 class EventListFormHelper(FormHelper):
@@ -34,6 +34,15 @@ class EventListFormHelper(FormHelper):
         ),
     )
 
+class PictureForm(forms.ModelForm):
+    image = forms.ImageField()
+
+    class Meta:
+        model = Picture
+        fields = [
+            "description",
+            "image",
+        ]
 
 class EventForm(forms.ModelForm):
     """
