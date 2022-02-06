21124dcd729c9d94482d98273dba1b5ea14aefb0
Franklin Ventura <franklin.ventura@roche.com>
2022-02-05 22:40:36 -0700

    Expand user import for email and user_id case match

--- thetatauCMT/users/resources.py
+++ thetatauCMT/users/resources.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.core.exceptions import ValidationError
 from import_export import resources
 from import_export.fields import Field
@@ -26,8 +27,8 @@ class UserResource(resources.ModelResource):
         model = User
         skip_unchanged = True
         report_skipped = False
-        import_id_fields = ("user_id",)
-        fields = ("user_id",)
+        import_id_fields = ("user_id", "email")
+        exclude = ("user_id",)
 
     def before_import(self, dataset, using_transactions, dry_run, **kwargs):
         dataset.headers = [header.strip() for header in dataset.headers]
@@ -40,17 +41,36 @@ class UserResource(resources.ModelResource):
             attrs,
         )
         self.fields = new.fields
-        # self.fields.pop("field_name")
-        # for header in dataset.headers:
-        #     if header == "user_id":
-        #         continue
-        #     field = self.field_from_django_field(
-        #         header, User._meta.get_field(header), readonly=False
-        #     )
-        #     self.fields[header] = field
 
     def init_instance(self, row=None):
         raise ValidationError(f"There is no user with id {row['user_id']}")
 
     def after_import(self, dataset, result, using_transactions, dry_run, **kwargs):
         result.diff_headers = self.get_diff_headers()
+
+    def get_instance(self, instance_loader, row):
+        queries = []
+        if "user_id" in row:
+            queries.append(Q(user_id__iexact=row["user_id"]))
+        if "email" in row:
+            queries.append(Q(email_school__iexact=row["email"]))
+            queries.append(Q(email__iexact=row["email"]))
+        # Take one Q object from the list
+        query = queries.pop()
+        # Or the Q object with the ones remaining in the list
+        for item in queries:
+            query |= item
+        try:
+            instance = instance_loader.get_queryset().get(query)
+        except self.Meta.model.DoesNotExist:
+            return None
+        except self.Meta.model.MultipleObjectsReturned:
+            raise ValidationError(f"Multiple users found for {row}")
+        else:
+            return instance
+
+    def before_import_row(self, row, row_number=None, **kwargs):
+        if "user_id" in row:
+            row["user_id"] = row["user_id"].strip()
+        if "email" in row:
+            row["email"] = row["email"].strip()
