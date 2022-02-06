f710b17b2e66de8a6bb20000628fac96694d3491
Franklin Ventura <franklin.ventura@roche.com>
2022-02-05 21:06:29 -0700

    Add bulk update for Users

--- thetatauCMT/users/resources.py
+++ thetatauCMT/users/resources.py
@@ -1,6 +1,7 @@
+from django.core.exceptions import ValidationError
 from import_export import resources
 from import_export.fields import Field
-from .models import UserRoleChange
+from .models import UserRoleChange, User
 
 
 class UserRoleChangeResource(resources.ModelResource):
@@ -16,3 +17,40 @@ class UserRoleChangeResource(resources.ModelResource):
             "start",
             "end",
         )
+
+
+class UserResource(resources.ModelResource):
+    field_name = Field(column_name="field_name")
+
+    class Meta:
+        model = User
+        skip_unchanged = True
+        report_skipped = False
+        import_id_fields = ("user_id",)
+        fields = ("user_id",)
+
+    def before_import(self, dataset, using_transactions, dry_run, **kwargs):
+        dataset.headers = [header.strip() for header in dataset.headers]
+        UserResource.Meta.fields = dataset.headers
+        attrs = dict(vars(UserResource))
+        new = resources.ModelDeclarativeMetaclass.__new__(
+            resources.ModelDeclarativeMetaclass,
+            "UserResource",
+            (resources.ModelResource,),
+            attrs,
+        )
+        self.fields = new.fields
+        # self.fields.pop("field_name")
+        # for header in dataset.headers:
+        #     if header == "user_id":
+        #         continue
+        #     field = self.field_from_django_field(
+        #         header, User._meta.get_field(header), readonly=False
+        #     )
+        #     self.fields[header] = field
+
+    def init_instance(self, row=None):
+        raise ValidationError(f"There is no user with id {row['user_id']}")
+
+    def after_import(self, dataset, result, using_transactions, dry_run, **kwargs):
+        result.diff_headers = self.get_diff_headers()
