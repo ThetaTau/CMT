4d3eec3a11a082a49d19823737a44d8543b656d6
Franklin Ventura <Franklin.Ventura@roche.com>
2022-09-11 13:03:09 -0700

    Add signal define email to notify

--- core/signals.py
+++ core/signals.py
@@ -1,4 +1,6 @@
 from django.contrib.contenttypes.models import ContentType
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from email_signals.models import Signal, SignalConstraint
 
 CHAPTER_WATCHES = [
@@ -40,36 +42,50 @@ class SignalWatchMixin:
     object_type = "not_set"
 
     def watch_notification_add(self, request, queryset):
-        if self.object_type == "user":
-            watch_types = USER_WATCHES
-        else:
-            watch_types = CHAPTER_WATCHES
-        for entity in queryset:
-            for watch_type in watch_types:
-                app, model, constraint_param = watch_type
-                content_type = ContentType.objects.filter(
-                    app_label=app, model=model
-                ).first()
-                signal, created = Signal.objects.update_or_create(
-                    name=f"{self.object_type} {entity} watch {app} {model}".title(),
-                    defaults=dict(
+        if "apply" in request.POST:
+            if self.object_type == "user":
+                watch_types = USER_WATCHES
+            else:
+                watch_types = CHAPTER_WATCHES
+            email = request.POST.get("emails", "central.office@thetatau.org")
+            for entity in queryset:
+                for watch_type in watch_types:
+                    app, model, constraint_param = watch_type
+                    content_type = ContentType.objects.filter(
+                        app_label=app, model=model
+                    ).first()
+                    signal, created = Signal.objects.update_or_create(
                         name=f"{self.object_type} {entity} watch {app} {model}".title(),
-                        plain_message=f"This message is to inform you that {self.object_type} {entity} watch {app} {model} was updated. In H&T, Chapter Management Tool",
-                        description=f"{self.object_type} {entity} watch {app} {model}".title(),
-                        content_type=content_type,
-                        subject=f"{self.object_type} {entity} {app} {model} updated".title(),
-                        mailing_list="central.office@thetatau.org",
-                        template="herald/html/signal_watch.html",
-                        signal_type="post_save",
-                    ),
-                )
-                if created:
-                    SignalConstraint(
-                        signal=signal,
-                        param_1=constraint_param,
-                        comparison="exact",
-                        param_2=entity.id,
-                    ).save()
+                        defaults=dict(
+                            name=f"{self.object_type} {entity} watch {app} {model}".title(),
+                            plain_message=f"This message is to inform you that {self.object_type} {entity} watch {app} {model} was updated. In H&T, Chapter Management Tool",
+                            description=f"{self.object_type} {entity} watch {app} {model}".title(),
+                            content_type=content_type,
+                            subject=f"{self.object_type} {entity} {app} {model} updated".title(),
+                            mailing_list=email,
+                            template="herald/html/signal_watch.html",
+                            signal_type="post_save",
+                        ),
+                    )
+                    if created:
+                        SignalConstraint(
+                            signal=signal,
+                            param_1=constraint_param,
+                            comparison="exact",
+                            param_2=entity.id,
+                        ).save()
+                    else:
+                        signal.mailing_list = email
+                        signal.save()
+            self.message_user(
+                request, f"Added watch for {email} of {self.object_type}(s): {queryset}"
+            )
+            return HttpResponseRedirect(request.get_full_path())
+        return render(
+            request,
+            "admin/watch_intermediate.html",
+            context={"users": queryset.values_list("id", flat=True)},
+        )
 
     watch_notification_add.short_description = "Create Notification Watch"
 
@@ -86,5 +102,8 @@ class SignalWatchMixin:
                 )
                 if signal:
                     signal.first().delete()
+        self.message_user(
+            request, f"Removed watch of {self.object_type}(s): {queryset}"
+        )
 
     watch_notification_remove.short_description = "Remove Notification Watch"
