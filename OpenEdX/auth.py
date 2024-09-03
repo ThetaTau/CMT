213ff3e565a7bac1713292f52abce99f9f5526a7
Ventura, Franklin {DSRE~Tucson} <franklin.ventura@roche.com>
2024-09-02 18:48:39 -0700

    Fix auth for PKCE code config

--- OpenEdX/auth.py
+++ OpenEdX/auth.py
@@ -10,6 +10,9 @@ class CMTOAuth2(BaseOAuth2PKCE):
     ACCESS_TOKEN_METHOD = "POST"
     SCOPE_SEPARATOR = ","
     EXTRA_DATA = [("id", "id"), ("expires", "expires")]
+    PKCE_DEFAULT_CODE_CHALLENGE_METHOD = "s256"  # can be "plain" or "s256"
+    PKCE_DEFAULT_CODE_VERIFIER_LENGTH = 48  # must be b/w 43-127 chars
+    DEFAULT_USE_PKCE = True
 
     def get_user_details(self, response):
         """Return user details from CMT account"""
