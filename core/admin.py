43b318d869804827f1182b5eaf574da6fb14bace
Ventura, Franklin {DSRE~Tucson} <franklin.ventura@roche.com>
2026-05-19 10:18:51 -0700

    Fix error

--- core/admin.py
+++ core/admin.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from django.urls import reverse
 from django.utils.safestring import mark_safe
 from herald.admin import SentNotificationAdmin
+from herald.models import SentNotification
 from report_builder.admin import ReportAdmin
 
 
